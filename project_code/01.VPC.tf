3c1ad4dc4e35ea6d9cc8ce8c01344d7a8f971d5c
sage98K <showhoho09@gmail.com>
2025-02-03 16:37:30 +0900

    Update 01.VPC.tf
    프랑스 > 버지니아로 수정

--- project_code/01.VPC.tf
+++ project_code/01.VPC.tf
@@ -12,16 +12,16 @@ resource "aws_vpc" "seoul" {
 }
 
 provider "aws" {
-  alias  = "france"
-  region = "eu-west-3" # 프랑스 리전
+  alias  = "Virginia"
+  region = "us-east-1" # 미국 버지니아 리전
 }
 
-resource "aws_vpc" "france" {
-  provider             = aws.france
+resource "aws_vpc" "Virginia" {
+  provider             = aws.Virginia
   cidr_block           = "10.1.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "CV_VPC_France"
+    Name = "CV_VPC_Virginia"
   }
 }
